ee76c77a86c5dce8bef0b692974ec3226cb00c74
jovijulian <jovijulian2002@gmail.com>
2023-12-22 20:24:16 +0700

    show category and talent

--- src/js/routes.js
+++ src/js/routes.js
@@ -3,6 +3,7 @@ import HomePage from '../pages/home.f7';
 import LoginPage from '../pages/login.f7'
 import RegistPage from '../pages/regist.f7'
 import CustomerHome from '../pages/customer/home.f7'
+import Talent from '../pages/customer/listTalent.f7'
 
 var routes = [
   {
@@ -20,6 +21,10 @@ var routes = [
   {
     path: '/customer-home/',
     component: CustomerHome,
+  },
+  {
+    path: '/talent/:id',
+    component: Talent,
   },
 ];
 
