6a9996cb01b534b19f1cfd8331fee83d3b6973dc
jovijulian <jovijulian2002@gmail.com>
2023-12-22 22:17:05 +0700

    done

--- src/js/routes.js
+++ src/js/routes.js
@@ -4,6 +4,10 @@ import LoginPage from '../pages/login.f7'
 import RegistPage from '../pages/regist.f7'
 import CustomerHome from '../pages/customer/home.f7'
 import Talent from '../pages/customer/listTalent.f7'
+import Order from '../pages/customer/Order.f7'
+import Transaction from '../pages/customer/transaction.f7'
+import DetailTransaction from '../pages/customer/detailTransaction.f7'
+import Review from '../pages/customer/review.f7'
 
 var routes = [
   {
@@ -25,6 +29,22 @@ var routes = [
   {
     path: '/talent/:id',
     component: Talent,
+  },
+  {
+    path: '/order/:idTalent/:idCategory',
+    component: Order,
+  },
+  {
+    path: '/transaction/',
+    component: Transaction,
+  },
+  {
+    path: '/detail-transaction/:transaction_id',
+    component: DetailTransaction,
+  },
+  {
+    path: '/review/:review_id',
+    component: Review,
   },
 ];
 
